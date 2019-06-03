18ec055afddb565a3bc8432bafffa6e219aedc77
Amilton <amilton.santana@b2wdigital.com>
2019-06-03 00:55:55 -0300

    feat: Exibir mais detalhes de um evento

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const botApiKey = require('./credentials/telegram.json').apiKey
 const event = require('./src/event')
 const state = require('./src/state')
 
-function start() {
+async function start() {
 
 	const bot = new TelegramBot(botApiKey, { polling: true })
 
-	bot.on('message', (msg) => {
+	bot.on('message', async (msg) => {
 		console.log(msg.text)
+		// console.log(JSON.stringify(msg))
+		// console.log(msg)
 
 		const userId = msg.from.id
 		const chatId = msg.chat.id
@@ -52,47 +54,65 @@ function start() {
 			if (userState && userState.context.subject && userState.context.subject == '/eventos') {
 				console.log('> Ja existe um userState para esse usuário.')
 				// console.log(`> Subject: ${userState.context.subject}`)
-				console.log('> Buscando a lista de eventos.');
-				const eventList = event.getList()
 
-				if (eventList) {
-					console.log('> Buscando a lista de tags existentes nos eventos.');
-					const eventTags = event.listEventsTags(eventList)
-					console.log(eventTags);
+				console.log('> Verificando se o usuário deseja mais detalhes sobre um evento.')
+				const reply = userState.context.children.find(child => child.message_id == msg.reply_to_message.message_id)
 
-					console.log(`> Verificando se a palavra informada (${msg.text}) é uma tag existente.`);
-					if (eventTags.find(tag => tag.match(new RegExp(msg.text)))) {
-						console.log(`> Tag ${msg.text} encontrada!`);
+				if (msg.reply_to_message && reply && msg.text.match(/mais detalhes/)) {
+					console.log('> Exibindo mais detalhes sobre o evento desejado.')
+					bot.sendMessage(chatId, reply.reply_text)
 
-						const events = event.findEventsByTag(msg.text)
+				} else {
+					console.log('> Buscando a lista de eventos.');
+					const eventList = event.getList()
 
-						const message = `Segue a lista dos eventos do tema #${msg.text}:`
-						bot.sendMessage(chatId, message, {
-							"reply_markup": {
-								"remove_keyboard": true
-							}
-						})
+					if (eventList) {
+						console.log('> Buscando a lista de tags existentes nos eventos.');
+						const eventTags = event.listEventsTags(eventList)
+						console.log(eventTags);
 
-						events.forEach((event) => {
-							let eventMsg = `${moment(event.initialDate).format('LLLL')} até às ${moment(event.endDate).format('LT')} terá o evento '${event.name}'`
-							eventMsg += `\n<b>Local</b>: ${event.place}`
-							eventMsg += `\n<b>Endereço:</b> ${event.address}`
-							bot.sendMessage(chatId, eventMsg, {
-								parse_mode: "HTML",
+						console.log(`> Verificando se a palavra informada (${msg.text}) é uma tag existente.`);
+						if (eventTags.find(tag => tag.match(new RegExp(msg.text)))) {
+							console.log(`> Tag ${msg.text} encontrada!`);
+
+							const events = event.findEventsByTag(msg.text)
+
+							let message = `Segue a lista dos eventos do tema #${msg.text}:`
+							await bot.sendMessage(chatId, message, {
+								"reply_markup": {
+									"remove_keyboard": true
+								}
 							})
-						})
 
-						userState.context.subject = null
+							events.forEach((event) => {
+								let eventMsg = `${moment(event.initialDate).format('LLLL')} até às ${moment(event.endDate).format('LT')} terá o evento '${event.name}'`
+								eventMsg += `\n<b>Local</b>: ${event.place}`
+								eventMsg += `\n<b>Endereço:</b> ${event.address}`
 
-						state.saveUserState(userState)
+								bot.sendMessage(chatId, eventMsg, {
+									parse_mode: "HTML",
+								}).then(res => {
+									const child = {
+										message_id: res.message_id,
+										reply_text: event.description
+									}
+									userState.context.children.push(child)
 
+									state.saveUserState(userState)
+								})
+							})
+
+							message = `Se quiser saber mais detalhes de algum evento, basta responder o evento desejado com o mensagem 'mais detalhes'.`
+							bot.sendMessage(chatId, message)
+
+						} else {
+							const message = `${userName}. No momento, não temos eventos cadastrados com o tema '#${msg.text}'.`
+							bot.sendMessage(chatId, message)
+						}
 					} else {
-						const message = `${userName}. No momento, não temos eventos cadastrados com o tema '#${msg.text}'.`
+						const message = `Olá ${userName}. No momento, não temos eventos cadastrados.`
 						bot.sendMessage(chatId, message)
 					}
-				} else {
-					const message = `Olá ${userName}. No momento, não temos eventos cadastrados.`
-					bot.sendMessage(chatId, message)
 				}
 			} else {
 				const message = `Não entendi ${userName}. O que você deseja saber?`
