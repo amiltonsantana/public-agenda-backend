6ebc3d06201d307931a0195c149f74c774119570
Amilton <amilton.santana@b2wdigital.com>
2019-07-04 17:56:04 -0300

    style: Adequação do código ao padrão de estilos

--- backend/src/api/event/eventModel.js
+++ backend/src/api/event/eventModel.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const EventSchema = new mongoose.Schema({
-	name: String,
-	description: String,
-	place: String,
-	address: String,
-	initialDate: Date,
-	endDate: Date,
-	tags: [ String ],
-	link: String
+  name: String,
+  description: String,
+  place: String,
+  address: String,
+  initialDate: Date,
+  endDate: Date,
+  tags: [String],
+  link: String,
 }, {
-	timestamps: true
-})
+  timestamps: true,
+});
 
-module.exports = mongoose.model('Event', EventSchema)
+module.exports = mongoose.model('Event', EventSchema);
